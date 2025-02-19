0cde72e918835008555ea9c97e35c28c0fafb16c
atrepalin <atrepalin19@gmail.com>
2025-02-19 20:14:09 +0300

    Add jupyter for more interactivity

--- main.py
+++
@@ -1,7 +0,0 @@
-from lib.visualize import plot, Layout
-
-adj_matrix = [[6, 4, 0, 0], [3, 0, 0, 7], [2, 9, 0, 0], [1, 0, 5, 0]]
-
-# adj_matrix = [[0, 0, 0, 0], [3, 0, 0, 0], [2, 9, 0, 0], [1, 0, 5, 0]]
-
-plot(adj_matrix, Layout.kamada_kawai)
